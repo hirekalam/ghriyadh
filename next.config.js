58ee50c704d4a86df6cc23f361b4ffc79f71f63f
cyberianv <kalamansari1996@gmail.com>
2026-08-23 04:27:38 +0300

    npx

--- next.config.js
+++ next.config.js
@@ -8,11 +8,6 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  // Ensure Next.js knows the src/ directory is the project root
-  // and that we are using the App Router exclusively
-  experimental: {
-    appDir: true,
-  },
 };
 
 export default nextConfig;
