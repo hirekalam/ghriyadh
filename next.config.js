0f0fbf3173af78dca9861050aa33ba2226a56e9b
cyberianv <kalamansari1996@gmail.com>
2026-08-23 04:51:19 +0300

    ok

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Suppress build-blocking type/lint errors (they are handled separately)
+  output: 'dist',
   typescript: {
     ignoreBuildErrors: true,
   },
